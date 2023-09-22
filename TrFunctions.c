19bd233fa65ee6c4a69871d004c9e238c175d34d
Cyrill-A-NY <cyrill.weber@noseryoung.com>
2023-09-22 12:13:26 +0200

    made the rps replayabel

--- TrFunctions.c
+++ TrFunctions.c
@@ -4,64 +4,70 @@
 
 int PaperStoneSissors()
 {
-    int playerguess = 0;
-    srand(time(NULL));
-    int randomnumber = (rand() % (3 - 1 + 1)) + 1;
+    char playrps = 'y';
+    while (playrps = 'y')
+    {
+        int playerguess = 0;
+        srand(time(NULL));
+        int randomnumber = (rand() % (3 - 1 + 1)) + 1;
 
-    printf("%d", randomnumber);
-    scanf("%d", &playerguess);
-/*
-    if (randomnumber == playerguess && playerguess == 1)
-    {
-        if (randomnumber == 1)
+        printf("\e[33mRock Paper Sissors\e[0m\n\nEnter your Guess!\n<1> Schere\n<2> Stein\n<3> Papier\n");
+        scanf("%d", &playerguess);
+
+        if (playerguess == 1)
         {
-            printf("Gleichstand\n");
+            if (randomnumber == 1)
+            {
+                printf("\e[30mGleichstand\e[0m\n");
+            }
+            else if (randomnumber == 2)
+            {
+                printf("\e[31mDu verlierst!\e[0m\n");
+            }
+            else if (randomnumber == 3)
+            {
+                printf("\e[32mDu gewinnst!\e[0m\n");
+            }
         }
-        else if (randomnumber == 2)
+        else if (playerguess == 2)
         {
-            printf("Du verlierst!\n");
+            if (randomnumber == 1)
+            {
+                printf("\e[32mDu gewinnst!\e[0m\n");
+            }
+            else if (randomnumber == 2)
+            {
+                printf("\e[30mGleichstand\e[0m\n");
+            }
+            else if (randomnumber == 3)
+            {
+                printf("\e[31mDu verlierst!\e[0m\n");
+            }
         }
-        else if (randomnumber == 3)
+        else if (playerguess == 3)
         {
-            printf("Du gewinnst!\n");
+            if (randomnumber == 1)
+            {
+                printf("\e[31mDu verlierst!\e[0m\n");
+            }
+            else if (randomnumber == 2)
+            {
+                printf("\e[32mDu gewinnst!\e[0m\n");
+            }
+            else if (randomnumber == 3)
+            {
+                printf("\e[30mGleichstand\e[0m\n");
+            }
         }
-    }
-    else if (playerguess == 2)
-    {
-        if (randomnumber == 1)
+        else
         {
-            printf("Du gewinnst!\n");
+            printf("\e[31mError\e[0m\n");
         }
-        else if (randomnumber == 2)
-        {
-            printf("Gleichstand\n");
-        }
-        else if (randomnumber == 3)
-        {
-            printf("Du verlierst!\n");
-        }
-    }
-    else if (playerguess == 3)
-    {
-        if (randomnumber == 1)
-        {
-            printf("Du verlierst!\n");
-        }
-        else if (randomnumber == 2)
-        {
-            printf("Du Gewinnst!\n");
-        }
-        else if (randomnumber == 3)
-        {
-            printf("Gleichstand\n");
-        }
-    }
-    else
-    {
-        printf("Error\n");
+        printf("Wilst du nochmal spielen? (y/n)\n");
+        scanf("%c", &playrps);
+
     }
 }
-
 
 int subtract(int num1, int num2)
 {
@@ -76,7 +82,7 @@ int subtract(int num1, int num2)
     else
     {
         printf("Die Zahl ist nicht negativ.\n");
-    } 
+    }
 }
 
 int multiplication(int num1, int num2)
@@ -87,9 +93,11 @@ int multiplication(int num1, int num2)
 
 int Expo(int base, int expo)
 {
-      int result = 1;
-    while (expo > 0) {
-        if (expo % 2 == 1) {
+    int result = 1;
+    while (expo > 0)
+    {
+        if (expo % 2 == 1)
+        {
             result *= base;
         }
         base *= base;
