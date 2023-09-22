55e321a8a3b42eb3f92621100215bf08730c5964
AnikSta <145564994+AnikSta@users.noreply.github.com>
2023-09-22 09:10:24 +0200

    added return

--- TrFunctions.c
+++ TrFunctions.c
@@ -22,14 +22,17 @@ int Testfunktion(int input){
 int subtract(int num1, int num2)
 {
    int subtract = num1 - num2;
+   return subtract;
 }
 
 int multiplication(int num1, int num2)
 {
     int multiplication = num1 * num2;
+    return multiplication;
 }
 
 int Expo( int num1, int x)
 {
     int Expo = num1^x;
+    return Expo;
 }
